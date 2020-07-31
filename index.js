ff73b4d9d9d5ffa850841bc60766eaa9c5bbb5bb
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-07-31 15:47:02 +0300

    some refactoring

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let tasks = [
-    { id: Math.random(), text: 'Buy milk', done: true },
+    { id: Math.random(), text: 'Buy milk', done: false },
     { id: Math.random(), text: 'Pick up Tom from airport', done: false },
     { id: Math.random(), text: 'Visit party', done: false },
     { id: Math.random(), text: 'Visit doctor', done: true },
@@ -8,26 +8,24 @@ let tasks = [
 
 const listElem = document.querySelector('.list');
 
-const createListItems = (id, text, isDone) => {
-    const checkboxClassName = isDone ? 'list__item_done' : '';
-    const isChecked = isDone ? 'checked' : '';
+const renderTasks = () => {
+    const createListItems = (id, text, isDone) => {
+        const checkboxClassName = isDone ? 'list__item_done' : '';
+        const isChecked = isDone ? 'checked' : '';
 
-    const newItem = `<li class="list__item ${checkboxClassName}" data-id=${id}>
-                        <input class="list__item-checkbox" type="checkbox" ${isChecked}>
-                        ${text}
-                    </li>`;
-    return newItem;
-};
-
-const renderListItems = () => {
+        const newItem = `<li class="list__item ${checkboxClassName}" data-id=${id}>
+                            <input class="list__item-checkbox" type="checkbox" ${isChecked}>
+                            ${text}
+                        </li>`;
+        return newItem;
+    };
     const todoListItems = tasks
         .sort((a, b) => a.done - b.done)
         .map(({ id, text, done }) => createListItems(id, text, done))
         .join('');
     return listElem.innerHTML = todoListItems;
 }
-
-renderListItems();
+renderTasks();
 
 const onStatusTodoChange = event => {
     const isCheckbox = event.target.classList.contains('list__item-checkbox');
@@ -35,28 +33,26 @@ const onStatusTodoChange = event => {
     const currentItem = event.target.closest('.list__item');
     const currentIndex = tasks.findIndex(item => item.id === +currentItem.dataset.id)
     const currentTodo = tasks[currentIndex];
-    currentTodo.done = !currentTodo.done;                    
-    return renderListItems();
+    currentTodo.done = !currentTodo.done;
+    return renderTasks();
 }
-
 listElem.addEventListener('click', onStatusTodoChange)
 
-const createNewTodo = text => {
-    const newItem = {
-        id: Math.random(),
-        text,
-        done: false
-    };
-    tasks.push(newItem);
-    return renderListItems();
-}
-
 const onAddTodo = () => {
+    const createNewTodo = text => {
+        const newItem = {
+            id: Math.random(),
+            text,
+            done: false
+        };
+        tasks.push(newItem);
+        return renderTasks();
+    }
     const inputTodoCreator = document.querySelector('.task-input');
     if (inputTodoCreator.value === '') return;
     createNewTodo(inputTodoCreator.value);
     inputTodoCreator.value = '';
-    return renderListItems();
+    return renderTasks();
 }
 
 const createTodoBtn = document.querySelector('.create-task-btn');
