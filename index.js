bfad99fbdaec490e219ca8c7db98164b05793fff
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 08:58:56 +0300

    not final variant

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
-let tasks = [
-    { id: Math.random(), text: 'Buy milk', done: false },
-    { id: Math.random(), text: 'Pick up Tom from airport', done: false },
-    { id: Math.random(), text: 'Visit party', done: false },
-    { id: Math.random(), text: 'Visit doctor', done: true },
-    { id: Math.random(), text: 'Buy meat', done: true },
+const tasks = [
+    { id: Math.random().toString().slice(2), text: 'Buy milk', done: false },
+    { id: Math.random().toString().slice(2), text: 'Pick up Tom from airport', done: false },
+    { id: Math.random().toString().slice(2), text: 'Visit party', done: false },
+    { id: Math.random().toString().slice(2), text: 'Visit doctor', done: true },
+    { id: Math.random().toString().slice(2), text: 'Buy meat', done: true },
 ];
 
 const listElem = document.querySelector('.list');
 
 const renderTasks = () => {
-    const createListItems = (id, text, isDone) => {
-        const checkboxClassName = isDone ? 'list__item_done' : '';
-        const isChecked = isDone ? 'checked' : '';
-
-        const newItem = `<li class="list__item ${checkboxClassName}" data-id=${id}>
-                            <input class="list__item-checkbox" type="checkbox" ${isChecked}>
-                            ${text}
-                        </li>`;
-        return newItem;
-    };
     const todoListItems = tasks
         .sort((a, b) => a.done - b.done)
-        .map(({ id, text, done }) => createListItems(id, text, done))
-        .join('');
-    return listElem.innerHTML = todoListItems;
+        .map(({ id, text, done }) => {
+            const newTaskItem = document.createElement('li');
+            newTaskItem.classList.add(`list__item`);
+            newTaskItem.dataset.id = id;
+            if (done) {
+                newTaskItem.classList.add('list__item_done');
+            }
+            
+            const newCheckboxItem = document.createElement('input');
+            newCheckboxItem.classList.add('list__item-checkbox');
+            newCheckboxItem.setAttribute('type', 'checkbox');
+            newCheckboxItem.checked = done;
+            
+            newTaskItem.append(newCheckboxItem, text); 
+            return newTaskItem;
+        });
+
+        listElem.innerHTML = '';
+        listElem.append(...todoListItems);
 }
 renderTasks();
 
 const onStatusTodoChange = event => {
     const isCheckbox = event.target.classList.contains('list__item-checkbox');
-    if (!isCheckbox) return;
-    const currentItem = event.target.closest('.list__item');
-    const currentIndex = tasks.findIndex(item => item.id === +currentItem.dataset.id)
-    const currentTodo = tasks[currentIndex];
-    currentTodo.done = !currentTodo.done;
-    return renderTasks();
-}
-listElem.addEventListener('click', onStatusTodoChange)
+    
+    if (!isCheckbox) {
+        return;
+    };
 
-const onAddTodo = () => {
-    const createNewTodo = text => {
-        const newItem = {
-            id: Math.random(),
-            text,
-            done: false
-        };
-        tasks.push(newItem);
-        return renderTasks();
-    }
+    const currentItem = event.target.closest('.list__item');
+    const currentTask = tasks.find(item => item.id === currentItem.dataset.id);
+    currentTask.done = !currentTask.done;
+    
+    renderTasks();
+};
+
+listElem.addEventListener('click', onStatusTodoChange);
+
+const onCreateTask = () => {
     const inputTodoCreator = document.querySelector('.task-input');
-    if (inputTodoCreator.value === '') return;
-    createNewTodo(inputTodoCreator.value);
+    
+    if (inputTodoCreator.value === '') {
+        return;
+    };
+
+    const newTask = {
+        id: Math.random().toString().slice(2),
+        text: inputTodoCreator.value,
+        done: false
+    };
+    tasks.push(newTask);
     inputTodoCreator.value = '';
-    return renderTasks();
+    renderTasks();
 }
 
 const createTodoBtn = document.querySelector('.create-task-btn');
-createTodoBtn.addEventListener('click', onAddTodo)
+createTodoBtn.addEventListener('click', onCreateTask);
